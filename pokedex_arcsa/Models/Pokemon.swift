db16868da3324d9f25fffbfecfee2f47be6fa137
CesarVargas <cesarvarcard@gmail.com>
2020-02-06 19:26:20 -0600

    Adding View Controllers and Pokemon File Model

---
+++ pokedex_arcsa/Models/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  pokedex_arcsa
+//
+//  Created by Cesar on 06/02/20.
+//  Copyright © 2020 CesarVargas. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    let name: String
+}
